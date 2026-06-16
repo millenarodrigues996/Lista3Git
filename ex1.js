d6ee121e0a83daf3e5a277445c50166ffe63c024
unknown <Inovape@CBRANB155827.ifb.local>
2026-06-16 17:47:20 -0300

    Primeiro Exercicio

---
+++ ex1.js
@@ -0,0 +1,15 @@
+//Crie um script que declare duas variáveis com valores numéricos. Realize as quatro operações 
+// matemáticas básicas (soma, subtração, multiplicação e divisão) e exiba os resultados no console.
+
+const num1 = 10
+const num2 = 50
+
+const soma = num1 + num2
+const subtração = num1 - num2
+const multiplicação = num1 * num2
+const divisão = num1 / num2
+
+console.log(`Soma: ${num1} + ${num2} = ${soma}`)
+console.log(`Substração: ${num1} - ${num2} = ${subtração}`)
+console.log(`Multiplicação: ${num1} * ${num2} = ${multiplicação}`)
+console.log(`Divisão: ${num1} / ${num2} = ${divisão}`)
