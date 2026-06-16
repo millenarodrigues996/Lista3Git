fcd6e7063917842dcd5d7f6a1abc30e5c6e5735b
unknown <Inovape@CBRANB155827.ifb.local>
2026-06-16 17:54:26 -0300

    Segundo Exercicio

---
+++ ex2.js
@@ -0,0 +1,10 @@
+// Escreva um programa que armazene a idade de uma pessoa em uma variável. Use uma
+// estrutura condicional (if/else) para verificar se a pessoa tem 16 anos ou mais. Se tiver, exiba no console
+// "Você pode votar.". Caso contrário, exiba "Você ainda não pode votar.".
+
+let idade = 25
+if (idade >= 16) {
+    console.log("Você pode votar")
+} else {
+    console.log("Você ainda não pode votar")
+}
